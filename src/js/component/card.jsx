317074de7e671d7d4b4525b89a952040ea260d89
analia-brb <analia.brb@gmail.com>
2023-01-06 16:50:38 +0000

    Landing Page React

---
+++ src/js/component/card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Card = function () {
+    return (
+        <div className="card-group my-5">
+  <div className="card">
+    <img src="https://www.calliaweb.co.uk/wp-content/uploads/2015/10/600x400.jpg" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src="https://www.calliaweb.co.uk/wp-content/uploads/2015/10/600x400.jpg" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src="https://www.calliaweb.co.uk/wp-content/uploads/2015/10/600x400.jpg" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+    )
+};
+
+export default Card
---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,13 @@
+// Paso 1
+import React from "react";
+
+// Paso 2
+const Navbar = function () {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <h2 className="text-light">Star Bootstrap</h2>
+        </nav>
+    )
+}
+// Paso 3
+export default Navbar
